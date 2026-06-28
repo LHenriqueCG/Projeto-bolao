ce4499a9d695c951d8cc8807bd670a3dbf3e966e
Henrique Cabral <luizhcabralg@gmail.com>
2026-06-28 12:34:52 -0300

    fase 16 avos

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'bolao-copa-2026-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 // Instala e cacheia os assets principais
